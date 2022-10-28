c091dedf52e2b14e2886d908177a3706637b369a
Dara Singh <dara.singh@intelligaia.com>
2022-10-28 12:30:52 +0530

    Initial theme for RBC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./customBigCalendar.css"; 
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    id: 0,
+    title: "Board meeting",
+    start: new Date(2022, 9, 28, 9, 0, 0),
+    end: new Date(2022, 9, 28, 13, 0, 0),
+    resourceId: 1,
+  },
+  {
+    id: 1,
+    title: "MS training",
+    allDay: true,
+    start: new Date(2022, 9, 28, 14, 0, 0),
+    end: new Date(2022, 9, 28, 16, 30, 0),
+    resourceId: 2,
+  },
+  {
+    id: 2,
+    title: "Team lead meeting",
+    start: new Date(2022, 9, 28, 5, 30, 0),
+    end: new Date(2022, 9, 28, 6, 30, 0),
+    resourceId: 3,
+  },
+  {
+    id: 11,
+    title: "Birthday Party",
+    start: new Date(2022, 9, 27, 7, 0, 0),
+    end: new Date(2022, 9, 27, 10, 30, 0),
+    resourceId: 4,
+  },
+];
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 }
