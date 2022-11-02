909dd664f46fdef1509d1ed4f3d39a21ae1926a9
Kanwaljit Singh <kanwaljeet.nagi@intelligaia.com>
2022-11-02 21:32:46 +0530

    theme updates for V2

--- src/App.js
+++ src/App.js
@@ -1,69 +1,67 @@
-
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import "./customBigCalendar.css"; 
+import "./customBigCalendar.css";
 const localizer = momentLocalizer(moment);
 
 const events = [
   {
     id: 0,
     title: "Board meeting",
-    start: new Date(2022, 9, 28, 9, 0, 0),
-    end: new Date(2022, 9, 28, 13, 0, 0),
+    start: new Date(2022, 10, 28, 9, 0, 0),
+    end: new Date(2022, 10, 28, 13, 0, 0),
     resourceId: 1,
   },
   {
     id: 1,
     title: "MS training 1",
     allDay: true,
-    start: new Date(2022, 9, 28, 14, 0, 0),
-    end: new Date(2022, 9, 28, 16, 30, 0),
+    start: new Date(2022, 10, 28, 14, 0, 0),
+    end: new Date(2022, 10, 28, 16, 30, 0),
     resourceId: 2,
   },
   {
     id: 2,
     title: "MS training 2",
     allDay: true,
-    start: new Date(2022, 9, 28, 14, 0, 0),
-    end: new Date(2022, 9, 28, 16, 30, 0),
+    start: new Date(2022, 10, 28, 14, 0, 0),
+    end: new Date(2022, 10, 28, 16, 30, 0),
     resourceId: 2,
   },
   {
     id: 4,
     title: "MS training 3",
     allDay: true,
-    start: new Date(2022, 9, 28, 14, 0, 0),
-    end: new Date(2022, 9, 28, 16, 30, 0),
+    start: new Date(2022, 10, 28, 14, 0, 0),
+    end: new Date(2022, 10, 28, 16, 30, 0),
     resourceId: 2,
   },
   {
     id: 5,
     title: "MS training 4",
     allDay: true,
-    start: new Date(2022, 9, 28, 14, 0, 0),
-    end: new Date(2022, 9, 28, 16, 30, 0),
+    start: new Date(2022, 10, 28, 14, 0, 0),
+    end: new Date(2022, 10, 28, 16, 30, 0),
     resourceId: 2,
   },
   {
     id: 6,
     title: "Team lead meeting",
-    start: new Date(2022, 9, 28, 5, 30, 0),
-    end: new Date(2022, 9, 28, 6, 30, 0),
+    start: new Date(2022, 10, 28, 5, 30, 0),
+    end: new Date(2022, 10, 28, 6, 30, 0),
     resourceId: 3,
   },
   {
     id: 11,
     title: "Birthday Party",
-    start: new Date(2022, 9, 27, 7, 0, 0),
-    end: new Date(2022, 9, 27, 10, 30, 0),
+    start: new Date(2022, 10, 27, 7, 0, 0),
+    end: new Date(2022, 10, 27, 10, 30, 0),
     resourceId: 4,
   },
 ];
 
 function App() {
- 
   return (
     <div>
       <Calendar
